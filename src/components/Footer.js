7be83af995fd3fc7a4b1de3c2ff6a1f573ff4334
github username <github email address>
2018-03-24 22:53:49 -0400

    created footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+const Footer = () => {
+  return (
+    <footer className = "footer footer-default">
+      <div className = "container">
+        <nav>
+          <ul>
+            <li>
+              <a href = 'http://www.acodercalledcole.com' className = "">
+                acodercalledcole.com
+              </a>
+            </li>
+            <li>
+              <a>
+                
+              </a>
+            </li>
+            <li></li>
+            <li></li>
+          </ul>
+        </nav>
+        <div className = "copyright">
+          &copy; 2018, All Rights Reserved
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer;
