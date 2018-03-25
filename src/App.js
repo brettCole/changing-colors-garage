6e2fec056761eceeb6677feef5ab7d8ae6d97ed1
github username <github email address>
2018-03-24 22:51:11 -0400

    added routes and css files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import LandingPage from './containers/LandingPage';
+import RepairPage from './containers/RepairPage';
+import ScheduleAppointment from './containers/ScheduleAppointment';
+import LocationPage from './containers/LocationPage';
+import AboutUsPage from './containers/AboutUsPage';
+import TestimonialPage from './containers/TestimonialPage';
 import './App.css';
 import './assets/css/now-ui-kit.css';
 
@@ -9,7 +14,30 @@ class App extends Component {
     return (
       <Router>
         <div className="App">
-          <Route exact path="/" component = { LandingPage } />
+          <Route 
+            exact path="/" 
+            component={LandingPage}
+          />
+          <Route 
+            path="/repairs"
+            component={RepairPage}
+          />
+          <Route 
+            path="/schedule-appointment"
+            component={ScheduleAppointment}
+          />
+          <Route
+            path="/location"
+            component={LocationPage}
+          />
+          <Route 
+            path="/aboutus"
+            component={AboutUsPage}
+          />
+          <Route
+            path="/testimonials"
+            component={TestimonialPage}
+          />
         </div>
       </Router>
     );
