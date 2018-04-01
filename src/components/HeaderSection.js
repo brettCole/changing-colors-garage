c1693b1b06ff7d4af1797d78e55ac38bdf5bc3e1
github username <github email address>
2018-04-01 19:09:27 -0400

    passing background image and title through props

--- src/components/HeaderSection.js
+++ src/components/HeaderSection.js
@@ -1,31 +1,35 @@
-import React, { Component } from 'react';
-import ChameleonMain from '../assets/images/GarageSlantDoor.jpg';
+import React from 'react';
+import { withRouter } from 'react-router';
 
-const HeaderSection = () => {
+const HeaderSection = (props) => {
   return (
     <div className = "page-header page-header-small">
       <div className = "page-header-image"
         data-parallax = "true"
-        style = {{backgroundImage: `url(${ ChameleonMain })`, transform: 'translate3d(0px, 0px, 0px)'}}>
+        style = {{backgroundImage: `url(${ props.background })`, transform: 'translate3d(0px, 0px, 0px)'}}>
       </div>
       <div className = "container">
         <div className = "content-center">
-          <h1 className = "title">Car repair done right the first time</h1>
-          <div className = "text-center">
-            <a href = "#" className = "btn btn-success btn-icon btn-round">
-              <i className = "fa fa-facebook-square"></i>
-            </a>
-            <a href = "#" className = "btn btn-success btn-icon btn-round">
-              <i className = "fa fa-twitter"></i>
-            </a>
-            <a href = "#" className = "btn btn-success btn-icon btn-round">
-              <i className = "fa fa-google-plus"></i>
-            </a>
-          </div>
+          <h1 className = "title">
+            { props.titleMessage }
+          </h1>
+          { props.location.pathname === '/' ? (
+            <div className = "text-center">
+              <a href = "#" className = "btn btn-success btn-icon btn-round">
+                <i className = "fa fa-facebook-square"></i>
+              </a>
+              <a href = "#" className = "btn btn-success btn-icon btn-round">
+                <i className = "fa fa-twitter"></i>
+              </a>
+              <a href = "#" className = "btn btn-success btn-icon btn-round">
+                <i className = "fa fa-google-plus"></i>
+              </a>
+            </div>
+          ) : null }
         </div>
       </div>
     </div>
   )
 }
 
-export default HeaderSection;
+export default withRouter(HeaderSection);
