461f17fe865326ef7bc6e9f4ab687ed74703a4b6
github username <github email address>
2018-04-01 19:08:43 -0400

    added components to landing page

--- src/containers/LandingPage.js
+++ src/containers/LandingPage.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
 import TopNavigation from '../components/TopNavigation';
 import HeaderSection from '../components/HeaderSection';
+import AboutUsSection from '../components/AboutUsSection';
+import TeamSection from '../components/TeamSection';
+import SignupEmail from '../components/SignupEmail';
+import Footer from '../components/Footer';
+import ChameleonMain from '../assets/images/GarageSlantDoor.jpg';
 
 class LandingPage extends Component {
-  render () {
+  render (props) {
     return (
       <div>
         <TopNavigation />
-        <HeaderSection />
+        <div className = "wrapper">
+          <HeaderSection 
+            background = { ChameleonMain }
+            titleMessage = "Car repair done right the first time."
+          />
+          <AboutUsSection />
+          <TeamSection />
+          <SignupEmail />
+          <Footer />
+        </div>
       </div>
     )
   }
