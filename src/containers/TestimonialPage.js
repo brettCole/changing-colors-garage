659394f3e8b718a6b685eca1a89bdb493f53b61d
github username <github email address>
2018-04-08 22:19:15 -0400

    created testimonial page container

---
+++ src/containers/TestimonialPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import TopNavigation from '../components/TopNavigation';
+import TestimonialSection from '../components/TestimonialSection';
+
+const TestimonialPage = () => {
+  return (
+    <div>
+      <TopNavigation />
+      <TestimonialSection />
+    </div>
+  )
+}
+
+export default TestimonialPage;
