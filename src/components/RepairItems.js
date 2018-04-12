3804c96857e2aa103c8824477499191e508bed20
github username <github email address>
2018-04-11 21:43:37 -0400

    refactored RepairDescriptions outside into RepairSection

--- src/components/RepairItems.js
+++ src/components/RepairItems.js
@@ -1,52 +1,21 @@
 import React from 'react';
+import { RepairDescriptions } from './RepairDescriptions';
 
-const RepairItems = (props) => {
-  debugger;
+export const RepairItems = (props) => {
+  
   return (
-    <div>
-      <div className="row">
-        <div className="col-md-5 ml-auto">
-          <div 
-            className="card card-background card-background-product card-raised" 
-            style={{backgroundImage: `url(${ props.cardImageLeft })`}}
-          >
-            <div className="card-body">
-              <h2 className="card-title">
-                {props.cardTitleLeft}
-              </h2>
-              <p className="card-description">
-                {props.cardDescriptionLeft}
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="col-md-5 mr-auto">
-          <div className="info info-horizontal"></div>
-        </div>
-      </div>
-      <hr />
-      <div className="row">
-        <div className="col-md-5 ml-auto mt-5">
-          <div className="info info-horizontal"></div>  
-        </div>
-        <div className="col-md-5 mr-auto">
-          <div 
-            className="card card-background card-background-product card-raised" 
-            style={{backgroundImage: `url(${ props.cardImageRight })`}}
-          >
-          <div className="card-body">
-            <h2 className="card-title">
-              {props.cardTitleRight}
-            </h2>
-            <p className="card-description">
-              {props.cardDescriptionRight}
-            </p>
-          </div>
-          </div>
-        </div>
+    <div 
+      className="card card-background card-background-product card-raised" 
+      style={{backgroundImage: `url(${ props.cardImage })`}}
+    >
+      <div className="card-body">
+        <h2 className="card-title">
+          {props.cardTitle}
+        </h2>
+        <p className="card-description">
+          {props.cardDescription}
+        </p>
       </div>
     </div>
   )
 }
-
-export default RepairItems;
