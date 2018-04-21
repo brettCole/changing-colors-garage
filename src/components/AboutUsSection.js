a2edc8397f1491b1541a25b305e1cc687ce0f647
github username <github email address>
2018-04-21 15:00:03 -0400

    rewrote about us section

--- src/components/AboutUsSection.js
+++ src/components/AboutUsSection.js
@@ -10,10 +10,10 @@ const AboutUsSection = () => {
         <div className = "row">
           <div className = "col-md-8 ml-auto mr-auto text-center">
             <h2 className = "title mb-4">
-              Keeping you on the road
+              Keeping you and your family safe on the road
             </h2>
             <h5 className = "description">
-              We've been keeping families safe on the road since 1995. Our priority is to fix cars not make money. The money is a result of being good at what we do.
+              We've been keeping families on the road since 1995. Our priority is to fix cars not make money. The money is a result of being good at what we do. We take pride in our name and our quality of work. Always have and always will.
             </h5>
           </div>
         </div>
@@ -37,10 +37,13 @@ const AboutUsSection = () => {
                   So what makes us different from every other Dealership, Repair Garage and grease monkey?
                 </h3>
                 <p>
-                  You know the routine, you take your car for a routine oil change. Not to your surprise here comes the service writer out to speak to you. He may start off with something like "We have a little bit of a problem...". Then he proceeds to give you a list of things that your vehicle needs this instant or you will likely die as soon as you pull off the lot. Don't you hate that!
+                  You know the routine, you take your car for a routine oil change. Not to your surprise here comes the service writer out to speak with you. He may start off with something like "We have a little bit of a problem...". Then he proceeds to give you a list of things that your vehicle needs this instant or you will likely die as soon as you pull off the lot. Don't you hate that!
                 </p>
                 <p>
-                  This is one of the ways that makes us different. We don't look to fluff the bill or push parts because we have to increase sales. We are in the business of fixing cars <i>"not changing parts"</i>. The fact remains though, parts on our vehicle's inevitably do wear out. 
+                  We don't look to fluff the bill or push parts because we have to increase sales. We are in the business of fixing cars <i>"not changing parts"</i>. When we speak with you, rest assured that you will receive you're moneys worth. We work hard for our reputation and we know how frustrating it is to find a good mechanic. 
+                </p>
+                <p>
+                  We look forward to working for you. Allow us to keep your vehicle on the road for the next 20 years.
                 </p>
             </div>
           </div>
