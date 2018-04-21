06480cce0d7f2b0ec7494b04d1c324ac3c42da59
github username <github email address>
2018-04-21 14:57:30 -0400

    added if statement to render icon or not

--- src/components/RepairDescriptions.js
+++ src/components/RepairDescriptions.js
@@ -3,17 +3,25 @@ import React from 'react';
 export const RepairDescriptions = (props) => {
   return (
     <React.Fragment>
-      <div className="info info-horizontal">
-        <div className="description">
-          <h4 className="info-title">
-            {props.info1Title}
-          </h4>
-          <ul className="description">
-            {props.info1List}
-          </ul>
+      {props.info1List !=='' &&
+        <div className="info info-horizontal">
+          <div className="icon icon-warning">
+            <i className="now-ui-icons ui-1_simple-remove" />
+          </div>
+          <div className="description">
+            <h4 className="info-title">
+              {props.info1Title}
+            </h4>
+            <ul className="description">
+              {props.info1List}
+            </ul>
+          </div>
         </div>
-      </div>
+      }
       <div className="info info-horizontal">
+        <div className="icon icon-success">
+          <i className="now-ui-icons ui-2_settings-90" />
+        </div>
         <div className="description">
           <h4 className="info-title">
             {props.info2Title}
@@ -23,7 +31,6 @@ export const RepairDescriptions = (props) => {
           </ul>
         </div>
       </div>
-      <div className="info info-horizontal"></div>
     </React.Fragment>
   )
 }
